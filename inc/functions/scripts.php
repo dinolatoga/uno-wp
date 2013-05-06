4f51c89e33dd5823d50265c76bf2d2b3edb554f4
Dino Latoga <dinolatoga@gmail.com>
2013-05-06 12:07:27 +0800

    add comment-reply scripts

--- inc/functions/scripts.php
+++ inc/functions/scripts.php
@@ -1,5 +1,5 @@
 <?php
-// register scripts
+// Use WordPress plugin to minify the scripts
 add_action('wp_enqueue_scripts', 'register_tsu_scripts');
 function register_tsu_scripts() {
 	// register plugins below
@@ -9,4 +9,8 @@ function register_tsu_scripts() {
 
 	// fire up the scripts
 	wp_enqueue_script( 'scripts');
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
